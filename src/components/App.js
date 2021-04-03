6f43652e1ddae30188507c5fc4c4ec3516ba7448
DESKTOP-3P9TOTC\cuby <cuby429.dev@gamil.com>
2021-04-03 04:56:37 +0300

    structured components, add buy feature

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,11 @@ class App extends Component {
 
   async loadWeb3() {
     if (window.ethereum) {
+      console.log(window.ethereum)
       window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
+      try {
+        await window.ethereum.enable()
+      } catch (error) {}
     }
     else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider)
@@ -30,9 +33,12 @@ class App extends Component {
     const web3 = window.web3
     // Load account
     const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
     this.setState({ account: accounts[0] })
     const networkId = await web3.eth.net.getId()
-    const networkData = Marketplace.networks[networkId]
+    console.log(networkId)
+    const networkData = Marketplace.networks[5777]
+    console.log(networkData)
     if(networkData) {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
       this.setState({ marketplace })
@@ -62,6 +68,7 @@ class App extends Component {
 
     this.createProduct = this.createProduct.bind(this)
     this.purchaseProduct = this.purchaseProduct.bind(this)
+    this.deleteProduct = this.deleteProduct.bind(this)
   }
 
   createProduct(name, price) {
@@ -79,7 +86,13 @@ class App extends Component {
       this.setState({ loading: false })
     })
   }
-
+  deleteProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.deleteProduct(id).send({ from: this.state.account, value: price })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
   render() {
     return (
       <div>
@@ -90,9 +103,11 @@ class App extends Component {
               { this.state.loading
                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
                 : <Main
-                  products={this.state.products}
-                  createProduct={this.createProduct}
-                  purchaseProduct={this.purchaseProduct} />
+                      products={this.state.products}
+                      createProduct={this.createProduct}
+                      purchaseProduct={this.purchaseProduct}
+                      deleteProduct={this.deleteProduct}
+                  />
               }
             </main>
           </div>
